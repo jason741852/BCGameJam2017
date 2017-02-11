19564b43fe7898f2b188c92eb6ea8dc83aa337d8
jason741852 <jason741852@gmail.com>
2017-02-10 23:08:24 -0800

    Add GameFinish script

---
+++ Assets/Scripts/FinishCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hi");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.CompareTag("Finish")){
+			//TODO: action
+		}
+	}
+}
