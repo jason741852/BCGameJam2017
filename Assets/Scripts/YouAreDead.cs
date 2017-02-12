6ea6a6e4f292ee80c02368f53ad08614c6bbde16
Jason Chan <jason741852@gmail.com>
2017-02-12 03:29:22 -0800

    Add trap and youAreDead script

---
+++ Assets/Scripts/YouAreDead.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouAreDead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+        {
+            player.gameObject.GetComponent<PlayerContoller>().Die();
+        }
+    }
+}
